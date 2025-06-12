802a89ef618055a6d1e240ffa797faea54e51a6a
ValenFar <maxy.far@gmail.com>
2025-06-11 21:33:10 -0300

    Creacion del hook useFetchProducts, Context ProductContext, ProductCard y detalles, tambien su incorporacion en App.jsx y main.jsx  - MaximoValentinFarfanAntileo

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,25 @@ import AppRouter from "./router/Router";
 import NavBar2 from "./layouts/Navbar2";
 import { Container, Row, Col } from "react-bootstrap";
 import Footer from "./layouts/Footer";
-
+import useFetchProducts from "./hooks/useFetchProducts";
 
 function App() {
   return (
-    
-    <Container
-      fluid
-      className="bg-app min-vh-100 d-flex flex-column"
-    >
+    <Container fluid className="bg-app min-vh-100 d-flex flex-column">
       <Row className="flex-grow-1 d-flex justify-content-center align-items-center">
         <Col xs={12} md={11} lg={10} xl={10} xxl={9}>
           <div
-            className="bg-white rounded shadow p-3"
+            className="bg-white rounded shadow p-3 m-3"
             style={{ minHeight: "90vh" }}
           >
             <NavBar2 />
             <AppRouter />
-            
-            
-      <Footer/>
+
+            <Footer />
           </div>
         </Col>
       </Row>
     </Container>
-
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ProductProvider } from "./context/ProductContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ProductProvider>
+      <App />
+    </ProductProvider>
+  </BrowserRouter>
+);
